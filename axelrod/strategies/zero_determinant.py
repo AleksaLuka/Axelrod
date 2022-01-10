ce41f2640fabc5352dfe4339da9e01601f722e75
AleksaLuka <aleksa.zizic18@imperial.ac.uk>
2022-01-10 16:57:28 +0000

    Update zero_determinant.py
    nope that did not help

--- axelrod/strategies/zero_determinant.py
+++ axelrod/strategies/zero_determinant.py
@@ -2,7 +2,6 @@ from axelrod.action import Action
 
 from .memoryone import MemoryOnePlayer
 
-import random
 
 C, D = Action.C, Action.D
 
@@ -265,54 +264,3 @@ class ZDSet2(LRPlayer):
         self, phi: float = 1 / 4, s: float = 0.0, l: float = 2
     ) -> None:
         super().__init__(phi, s, l)
-
-class MyStrategy(Player):
-
-    """
-    A strategy where the player goes back and forth between using
-    the normal TitForTat strategy and the Zero Determinant - Generous
-    TitForTat strategy.
-    """
-
-    name = "MyStrategy"
-
-    classifier = {
-        "memory_depth": 1,
-        "stochastic": False,
-        "long_run_time": False,
-        "inspects_source": False,
-        "manipulates_source": False,
-        "manipulates_state": False,
-    }
-
-    def strategy(opponent):
-
-        # nice strategy, so start with C
-        if len(self.history) == 0:
-            return C
-
-        # play TFT
-        elif ((len(self.history)/30)%2) == 0:
-            return opponent.history[-1]
-
-        # play ZD-GTFT2
-        else:
-            if (opponent.history[-1] == C and self.history[-1] == C):
-                return C
-
-            elif (opponent.history[-1] == D and self.history[-1] == C):
-                p = random.random()
-                if (p <= 0.125):
-                    return C
-                else:
-                    return D
-
-            elif (opponent.history[-1] == C and self.history[-1] == D):
-                return C
-
-            else:
-                p = random.random()
-                if (p <= 0.25):
-                    return C
-                else:
-                    return D
